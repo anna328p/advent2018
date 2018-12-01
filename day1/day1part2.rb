5eaa8bde9b4814333e4b23e9aecd5844f3360236
Dmitry Kudriavtsev <me@dk0.us>
2018-11-30 23:35:29 -0800

    found out about Enumerator#cycle

--- day1/day1part2.rb
+++ day1/day1part2.rb
@@ -10,16 +10,13 @@ a = s.split.map(&:to_i)
 
 r = 0
 h = Hash.new(0)
-found = false
 
-until found do
-  a.each do |i|
-    r += i
-    h[r] += 1
-    if h[r] > 1
-      puts r
-      found = true
-      break
-    end
+a.cycle do |i|
+  r += i
+  h[r] += 1
+  if h[r] > 1
+    puts r
+    found = true
+    break
   end
 end
