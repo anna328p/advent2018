ca241435ea9599cb626700413465a3de48a161c6
Dmitry Kudriavtsev <dkudriavtsev@gmail.com>
2018-12-05 17:51:02 -0800

    day 3 part 2-ish

---
+++ day3/day3part2.md
@@ -0,0 +1,3 @@
+# day 3 part 2
+
+use the generate_image.rb to generate an image, then find the non-overlapping rectangle.
--- day3/generate_image.rb
+++ day3/generate_image.rb
@@ -2,30 +2,24 @@
 
 # Day 3 Part 1
 
-class Array
-  def inc
-    self.map &:inc
-  end
-end
-
-class Integer
-  def inc
-    self + 1
-  end
-end
-
-s = File.read('input').split(?\n)
+file = File.readlines 'input'
 `convert -size 1000x1000 xc:white output.png`
 
-cmdstring = ""
+rect_cmds, label_cmds = '', ''
 
-a = Array.new(1000) { Array.new(1000, 0) }
-s.map { |s|
+file.map { |s|
   /#(?<id>\d+) @ (?<x>\d+),(?<y>\d+): (?<width>\d+)x(?<height>\d+)/ =~ s
-  {id: id.to_i, x: x.to_i, y: y.to_i, w: width.to_i, h: height.to_i}
+  { id: id.to_i, x: x.to_i, y: y.to_i, w: width.to_i, h: height.to_i }
 }.each { |s|
-  cmdstring << "-draw \"rectangle #{s[:x]},#{s[:y]} #{s[:x]+s[:w]},#{s[:y]+s[:h]}\" "
+  rect_cmds << "-draw \"rectangle #{s[:x]},#{s[:y]} #{s[:x] + s[:w]},#{s[:y] + s[:h]}\" "
+  label_cmds << "-draw \" text #{s[:x]},#{s[:y]} '#{s[:id]}'\" "
   puts s
 }
 
-`convert output.png -strokewidth 0 -fill "rgba( 0, 0, 0, 0.1 )" #{cmdstring} output.png`
+`convert output.png \
+  -strokewidth 0 \
+  -fill "rgba( 0, 0, 0, 0.1 )" \
+  #{rect_cmds} \
+  -fill "rgba( 0, 0, 0, 1 )" \
+  #{label_cmds} \
+  output.png`
